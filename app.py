bf6b6e341617fbf9737119084c1466998a8b6b92
Grégoire Delattre <gregoire.delattre@gmail.com>
2015-03-25 21:05:14 +0100

    Remove country from guessit for now

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 
 @app.route("/guess/<filename>")
 def guess(filename):
-    return jsonify(guess_file_info(filename))
+    guess = guess_file_info(filename)
+
+    # Country is not unicode, it makes the jsonify crash...
+    # I don't need it anyway so let's remove it
+    if 'country' in guess:
+        del guess['country']
+
+    return jsonify(guess)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
