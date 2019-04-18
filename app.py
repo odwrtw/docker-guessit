322693b19e8204b86e77275cac1819f0199fe543
Grégoire Delattre <gregoire.delattre@gmail.com>
2019-04-19 00:58:53 +0200

    Append the country to the title

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ app = Flask(__name__)
 def guess(filename):
     guess = guessit(filename)
 
-    # Country is not unicode, it makes the jsonify crash...
-    # I don't need it anyway so let's remove it
+    # Country is a babelfish object and cannot be marshaled into JSON. We're
+    # not using the country anyway so let's append the country to the title.
+    # This also fixes wrong country match in the filename.
+    # e.g "Scandal.(US).S01E01.mp4" -> "Scanda US"
+    # e.g "This.is.us.S01E01.mp4"   -> "This is US"
     if 'country' in guess:
+        guess['title'] += ' ' + str(guess['country'])
         del guess['country']
 
     return jsonify(guess)
