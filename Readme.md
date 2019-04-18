7a1dd1564ac7d4b31273c56add0130a4e4da61d3
Grégoire Delattre <gregoire.delattre@gmail.com>
2019-04-18 12:55:29 +0200

    Add a POST method to handle the queries
    BTW, update guessit to 3.0.3

--- Readme.md
+++ Readme.md
@@ -2,18 +2,30 @@
 
 ### Build
 
-```
-docker build -t gregdel/guessit .
+```sh
+docker build -t guessit .
 ```
 
 ### Launch
 
-```
-docker run -it --rm -p 127.0.0.1:5000:5000 --name guessit gregdel/guessit
+```sh
+docker run -it --rm -p 127.0.0.1:5000:5000 --name guessit guessit
 ```
 
 ### Use
 
-```
+#### GET
+
+```sh
 curl http://localhost:5000/guess/your.filename.mp4
+```
+
+#### POST
+
+```sh
+curl \
+  -H "content-type: application/json" \
+  -X POST \
+  --data '{"name":"your.filename.mp4"}' \
+  http://127.0.0.1:5000/guess
 ```
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 from guessit import guessit
 
 app = Flask(__name__)
 
-@app.route("/guess/<filename>")
 def guess(filename):
     guess = guessit(filename)
 
@@ -14,6 +14,16 @@ def guess(filename):
         del guess['country']
 
     return jsonify(guess)
+
+@app.route("/guess", methods=['POST'])
+def guess_post():
+    content = request.get_json()
+    return guess(content['name'])
+
+@app.route("/guess/<filename>", methods=['GET'])
+def guess_get(filename):
+    return guess(filename)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
